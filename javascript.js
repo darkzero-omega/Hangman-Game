8ed8e60702e220ec532f5c7da93b2f4a747ab7e8
darkzero_omega <darkzero_omega2003@yahoo.com>
2018-02-14 21:36:22 -0500

    updated hangman

---
+++ javascript.js
@@ -0,0 +1,17 @@
+//Word Array
+const wordList = [ "Vega", "Ryu", "Chunli", "Hadoken", "Shoryuken", "Akuma", "Dragon Punch", "M.Bison", "Sagat"  ]
+
+//Letters
+const alphabet =[ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//Current Word
+var currentWord;
+
+//Word Randomizer
+var chosenWord = Math.floor(Math.random() * words.length);
+console.log(chosenWord);
+
+function gameStart() {
+    currentWord =  wordList[]
+
+}
